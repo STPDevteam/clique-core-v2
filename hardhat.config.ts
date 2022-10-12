f5334b0413af4b488aa2c9e595d9cbf57d9f286b
Tuyetlinhuiop05 <111103211+tuyetlinhuiop05@users.noreply.github.com>
2022-10-12 18:25:46 +0800

    feat: hardhat config add mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,9 @@ import {CHAINID} from "./constants/constants";
 require('dotenv').config({path: '.env'})
 
 const ETHERSCAN_API_KEYS: Map<number, string> = new Map<number, string>([
+    [CHAINID.ETH_MAINNET, `${process.env.apiKey}`],
     [CHAINID.ETH_GOERLI, `${process.env.apiKey}`],
+    [CHAINID.POLYGON, `${process.env.apiKeyPolygon}`],
     [CHAINID.POLYGON_MUMBAI, `${process.env.apiKeyPolygon}`]
 ]);
 const chainId = process.env.CHAINID ? Number(process.env.CHAINID) : 5;
@@ -66,12 +68,16 @@ const config: HardhatUserConfig = {
   namedAccounts: {
     deployer: {
       default: 5,
+      1: "0x60d1Ce3e4aC99f1f27f276A26BEeb4454d4f1161",
       5: "0x637856e617b168cF63C0A0E4FEf923be7C67FFcf",
+      137: "0x60d1Ce3e4aC99f1f27f276A26BEeb4454d4f1161",
       80001: "0x637856e617b168cF63C0A0E4FEf923be7C67FFcf",
     },
     admin: {
       default: 5,
+      1: "0x5931f4A88807d29B1732cAe52D5cEa6C3DE2119D",
       5: "0xbC86F047d37D29cB97ee7D860c5355A5f12c62d5",
+      137: "0x5931f4A88807d29B1732cAe52D5cEa6C3DE2119D",
       80001: "0xbC86F047d37D29cB97ee7D860c5355A5f12c62d5",
     }
   },
