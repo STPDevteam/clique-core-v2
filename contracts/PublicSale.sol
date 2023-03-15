8d804652f063d3e7c331f47ada0c27deb050fb8c
Anna <a4918795@gmail.com>
2023-03-15 10:40:04 +0800

    feat: add signature

--- contracts/PublicSale.sol
+++ contracts/PublicSale.sol
@@ -1,12 +1,21 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity =0.8.9;
 
+import './interfaces/IDAOFactory.sol';
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 
 contract PublicSale is Ownable2Step {
+
+    address public immutable factoryAddress;
     mapping(uint256 => sale) public sales;
+
+    constructor(address factoryAddress_) {
+        factoryAddress = factoryAddress_;
+    }
+
     struct sale {
         address creator;
         address saleToken;
@@ -36,13 +45,31 @@ contract PublicSale is Ownable2Step {
         uint256 _limitMax,
         uint256 _startTime,
         uint256 _endTime,
-        bytes calldata signature_
+        bytes calldata _signature
     ) external payable {
         require(_startTime < _endTime && _endTime > block.timestamp, "PublicSale: invalid duration.");
         require(_saleAmount > 0, "PublicSale: invalid sale amount.");
         require(_pricePer > 0, "PublicSale: invalid price.");
         require(_limitMin > 0 && _limitMax > 0 && _limitMax >= _limitMin, "PublicSale: invalid purchase.");
         require(_saleToken != address(0) && _receiveToken != address(0), "PublicSale: invalid token.");
+
+        bytes32 _hash = ECDSAUpgradeable.toEthSignedMessageHash(
+            keccak256(
+                abi.encodePacked(
+                    _msgSender(),
+                    _saleId,
+                    _saleToken,
+                    _saleAmount,
+                    _receiveToken,
+                    _pricePer,
+                    _limitMin,
+                    _limitMax,
+                    _startTime,
+                    _endTime
+                )
+            )
+        );
+        require(IDAOFactory(factoryAddress).isSigner(ECDSAUpgradeable.recover(_hash, _signature)), 'PublicSale: invalid signer.');
 
         sales[_saleId].creator = _msgSender();
         sales[_saleId].saleToken = _saleToken;
@@ -62,12 +89,23 @@ contract PublicSale is Ownable2Step {
     function Purchase(
         uint256 _saleId,
         uint256 _buyAmount,
-        bytes calldata signature_
+        bytes calldata _signature
     ) external payable {
         require(!sales[_saleId].isCancel, "PublicSale: invalid sale.");
         require(sales[_saleId].startTime < block.timestamp && sales[_saleId].endTime > block.timestamp, "PublicSale: invalid sale.");
         require(_buyAmount > 0 && _buyAmount >= sales[_saleId].limitMin && _buyAmount <= sales[_saleId].limitMax, "PublicSale: invalid amount.");
         require(sales[_saleId].soldedAmount + _buyAmount <= sales[_saleId].saleAmount, "PublicSale: invalid amount.");
+
+        bytes32 _hash = ECDSAUpgradeable.toEthSignedMessageHash(
+            keccak256(
+                abi.encodePacked(
+                    _msgSender(),
+                    _saleId,
+                    _buyAmount
+                )
+            )
+        );
+        require(IDAOFactory(factoryAddress).isSigner(ECDSAUpgradeable.recover(_hash, _signature)), 'PublicSale: invalid signer.');
 
         sales[_saleId].soldedAmount += _buyAmount;
 
